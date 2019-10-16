cd5503639761d51680efdc63941e6b8432017814
xinru <s3a632065@student.ncut.edu.tw>
2019-10-16 21:05:55 +0800

    初步建置三個路由(Routes)

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,29 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'web'], function () {
+
+    /**
+     * 顯示所有任務
+     */
+    Route::get('/', function () {
+        //
+    });
+
+    /**
+     * 增加新的任務
+     */
+    Route::post('/task', function (Request $request) {
+        //
+    });
+
+    /**
+     * 刪除任務
+     */
+    Route::delete('/task/{task}', function (Task $task) {
+        //
+    });
 });
