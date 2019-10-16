4292eb5113c1fc81aef9cb26acb75bba16b90108
xinru <s3a632065@student.ncut.edu.tw>
2019-10-16 21:25:50 +0800

    顯示&驗證錯誤訊息

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,10 @@ Route::group(['middleware' => 'web'], function () {
     Route::post('/task', function (Request $request) {
         // 驗證輸入
         $validator = Validator::make($request->all() , [
-            'name' => 'required|max:255',
+            'name' => 'required|max:255',       //驗證規則
         ]);
 
-        if ($validator->fails()) {
+        if ($validator->fails()) {      //驗證失敗的處理
             return redirect('/')
                 ->withInput()
                 ->withErrors($validator);
